fdb32f4f1fa817ee411cb81c29cec9e958d854ee
Blue <35626159+BlueElixir@users.noreply.github.com>
2022-04-09 11:00:02 +0300

    update: indicator
    added new indicators, very basic but gets the job done
    full list of new features:
    - moved from Anti-Aim box to a new one (still in the same tab)
    - added an antiaim side indicator, complete with custom colours
    - added a horizontal offset for the indicator

--- legit antiaim inverter/legitaa inverter.lua
+++ legit antiaim inverter/legitaa inverter.lua
@@ -1,10 +1,20 @@
-local aatab = gui.Reference("Legitbot", "Semirage", "Anti-Aim")
-local aatoggle = gui.Checkbox(aatab, "aatoggle", "Toggle Inverter", false)
-local aainv = gui.Keybox(aatab, "aainvkey", "Anti-Aim Inverter", 0)
+local aatab = gui.Reference("Legitbot", "Semirage")
+local aabox = gui.Groupbox(aatab, "Anti-Aim Inverter", 16, 360, 297, 200)
+local aatoggle = gui.Checkbox(aabox, "aatoggle", "Toggle Inverter", false)
+local aainv = gui.Keybox(aabox, "aainvkey", "Anti-Aim Inverter", 0)
+local aasideind = gui.Checkbox(aabox, "aaindicator", "Anti-Aim Side Indicator", false)
+local aaindcolour = gui.ColorPicker(aasideind, "aaindcolour", "Indicator Colour", 255, 255, 255, 255)
+local offset = gui.Slider(aabox, "aaindoffset", "Indicator Horizontal Offset", "0", "0", "100")
+local screenwidth, screenheight = draw.GetScreenSize()
+local leftind = screenwidth/2 - 50
+local rightind = screenwidth/2 + 30
+local indheight = screenheight/2-10
+local font = draw.CreateFont("Verdana", 30, 900)
 
 local function AAInverter()
     if aatoggle:GetValue() == true then
 		if aainv:GetValue() ~= 0 then
+			
 			gui.SetValue("lbot.antiaim.direction", 1)
 			if input.IsButtonPressed(aainv:GetValue()) then
 				if gui.GetValue("lbot.antiaim.leftkey") ~= 0 and gui.GetValue("lbot.antiaim.rightkey") == 0 then
@@ -21,6 +31,15 @@ local function AAInverter()
                         gui.SetValue("lbot.antiaim.rightkey", aainv:GetValue())
                     end
 				end
+			end
+		end
+		if aasideind:GetValue() == true then
+			draw.SetFont(font)
+			draw.Color(aaindcolour:GetValue())
+			if gui.GetValue("lbot.antiaim.leftkey") ~= 0 and gui.GetValue("lbot.antiaim.rightkey") == 0 then
+				draw.Text(rightind+offset:GetValue(), screenheight/2-10, ">")
+			elseif gui.GetValue("lbot.antiaim.leftkey") == 0 and gui.GetValue("lbot.antiaim.rightkey") ~= 0 then
+				draw.Text(leftind-offset:GetValue(), screenheight/2-10, "<")
 			end
 		end
     end
