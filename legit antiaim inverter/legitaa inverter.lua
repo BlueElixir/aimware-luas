d9d0f8b8bab4042007f7ca431b67056d9d81bea6
Blue <35626159+BlueElixir@users.noreply.github.com>
2022-04-09 11:58:35 +0300

    fixed indicators
    turns out i had plenty of time, here's the indicator fix
    fixes:
    - no longer changes indicator side when aimware menu is opened
    - doesn't break randomly
    
    new:
    - easy to change indicator symbols and font size/thickness, instructions at the top of the .lua file

--- legit antiaim inverter/legitaa inverter.lua
+++ legit antiaim inverter/legitaa inverter.lua
@@ -1,3 +1,19 @@
+-- CHANGE LEFT/RIGHT INDICATORS
+--
+-- if you want to set your own left/right indicator symbol,
+-- change the < and > below to whatever you want.
+-- find symbols to use at http://xahlee.info/comp/unicode_arrows.html
+-- some symbols might not work
+-- not recommended to use more than 1 symbol, it doesn't look nice
+--
+-- you can also change the size, though i recommend sticking with 30
+-- changing thickness is also not recommended, but if you want, go right ahead
+
+local leftIndicator = "<"				-- custom symbol, <, L, 🠈
+local rightIndicator = ">"				-- custom symbol, >, R, 🠊
+local indicatorSize = 30				-- any size, not recommended to change
+local thickness = 900					-- from 100 to 900
+
 local aatab = gui.Reference("Legitbot", "Semirage")
 local aabox = gui.Groupbox(aatab, "Anti-Aim Inverter", 16, 360, 297, 200)
 local aatoggle = gui.Checkbox(aabox, "aatoggle", "Toggle Inverter", false)
@@ -8,38 +24,58 @@ local offset = gui.Slider(aabox, "aaindoffset", "Indicator Offset", "0", "0", "2
 local screenwidth, screenheight = draw.GetScreenSize()
 local leftind = screenwidth/2 - 50
 local rightind = screenwidth/2 + 30
-local indheight = screenheight/2-10
-local font = draw.CreateFont("Verdana", 30, 900)
+local indheight = screenheight/2 - 10
+local font = draw.CreateFont("Verdana", indicatorSize, thickness)
+
+--vars
+local aaleft = "lbot.antiaim.leftkey"
+local aaright = "lbot.antiaim.rightkey"
+local value = 1
 
 local function AAInverter()
     if aatoggle:GetValue() == true then
 		if aainv:GetValue() ~= 0 then
-			
 			gui.SetValue("lbot.antiaim.direction", 1)
 			if input.IsButtonPressed(aainv:GetValue()) then
-				if gui.GetValue("lbot.antiaim.leftkey") ~= 0 and gui.GetValue("lbot.antiaim.rightkey") == 0 then
-					gui.SetValue("lbot.antiaim.leftkey", 0)
-					gui.SetValue("lbot.antiaim.rightkey", aainv:GetValue())
-				elseif gui.GetValue("lbot.antiaim.leftkey") == 0 and gui.GetValue("lbot.antiaim.rightkey") ~= 0 then
-					gui.SetValue("lbot.antiaim.leftkey", aainv:GetValue())
-					gui.SetValue("lbot.antiaim.rightkey", 0)
-                elseif gui.GetValue("lbot.antiaim.leftkey") == 0 and gui.GetValue("lbot.antiaim.rightkey") == 0 then
-                    local var = math.random(1, 100)
-                    if var > 50 then
-                        gui.SetValue("lbot.antiaim.leftkey", aainv:GetValue())
-                    else
-                        gui.SetValue("lbot.antiaim.rightkey", aainv:GetValue())
-                    end
+				if not gui.Reference("Menu"):IsActive() then
+					if value == 1 then
+						gui.SetValue(aaleft, 0)
+						gui.SetValue(aaright, aainv:GetValue())
+						value = 2
+					elseif value == 2 then
+						gui.SetValue(aaleft, aainv:GetValue())
+						gui.SetValue(aaright, 0)
+						value = 1
+      	          else
+     	               local var = math.random(1, 100)
+            	        if var > 50 then
+                 	    	gui.SetValue(aaleft, aainv:GetValue())
+							value = 1
+               		     else
+							gui.SetValue(aaright, aainv:GetValue())
+							value = 2
+						end
+					end
 				end
 			end
 		end
 		if aasideind:GetValue() == true then
 			draw.SetFont(font)
-			draw.Color(aaindcolour:GetValue())
-			if gui.GetValue("lbot.antiaim.leftkey") == 0 and gui.GetValue("lbot.antiaim.rightkey") ~= 0 then
-				draw.Text(rightind+offset:GetValue(), screenheight/2-10, ">")
-			elseif gui.GetValue("lbot.antiaim.leftkey") ~= 0 and gui.GetValue("lbot.antiaim.rightkey") == 0 then
-				draw.Text(leftind-offset:GetValue(), screenheight/2-10, "<")
+			
+			if value == 2 then
+				draw.Color(aaindcolour:GetValue())
+				draw.Text(rightind+offset:GetValue(), screenheight/2-10, rightIndicator)
+				draw.Color(107, 107, 107, 150)
+				draw.Text(leftind-offset:GetValue(), screenheight/2-10, leftIndicator)
+			elseif value == 1 then
+				draw.Color(aaindcolour:GetValue())
+				draw.Text(leftind-offset:GetValue(), screenheight/2-10, leftIndicator)
+				draw.Color(107, 107, 107, 150)
+				draw.Text(rightind+offset:GetValue(), screenheight/2-10, rightIndicator)
+			else
+				draw.Color(107, 107, 107, 150)
+				draw.Text(leftind-offset:GetValue(), screenheight/2-10, leftIndicator)
+				draw.Text(rightind+offset:GetValue(), screenheight/2-10, rightIndicator)
 			end
 		end
     end
