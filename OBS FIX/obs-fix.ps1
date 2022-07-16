0ac1ed3359ece5798baba0a6aeba7b21d0d7c5fa
Blue <35626159+BlueElixir@users.noreply.github.com>
2022-07-16 15:57:30 +0300

    fixed errors
    added messages that tell you to close CSGO and OBS before running script (otherwise it will start spitting errors)

--- OBS FIX/obs-fix.ps1
+++ OBS FIX/obs-fix.ps1
@@ -1,3 +1,14 @@
+$obs = Get-Process obs64 -ErrorAction SilentlyContinue
+$csgo = Get-Process csgo -ErrorAction SilentlyContinue
+if ($obs) {
+    Write-Host "     Close OBS.      " -BackgroundColor Red -ForegroundColor White
+    return
+}
+if ($csgo) {
+    Write-Host "     Close CSGO.     " -BackgroundColor Red -ForegroundColor White
+    return
+}
+
 $DesktopPath = [Environment]::GetFolderPath("Desktop")
 Write-Progress "got desktop path, downloading obs patch"
 
@@ -5,6 +16,8 @@ $sourcePatch = 'https://github.com/BlueElixir/aimware-luas/raw/main/OBS%20FIX/ob
 $sourceDLL = 'https://github.com/BlueElixir/aimware-luas/raw/main/OBS%20FIX/fixed_obs.zip'
 $destinationPatch = "$DesktopPath\obs-studio-hook.zip"
 $destinationDLL = "$DesktopPath\fixed_obs.zip"
+$destDLLwc = "C:\Program Files\obs-studio\obs-plugins\64bit\"
+$destDLLgh = "C:\Program Files\obs-studio\data\obs-plugins\win-capture\graphics-hook32.dll"
 
 Invoke-RestMethod -Uri $sourcePatch -OutFile $destinationPatch
 Invoke-RestMethod -Uri $sourceDLL -OutFile $destinationDLL
@@ -13,9 +26,12 @@ Write-Progress "downloaded obs patch, applying"
 Expand-Archive -Path "$DesktopPath\obs-studio-hook.zip" -DestinationPath "C:\Windows" -Force
 Write-Progress "applied patch, moving DLLs"
 
-Expand-Archive -Path "$DesktopPath\fixed_obs.zip" -DestinationPath "$DesktopPath" -Force
-Move-Item -Path "$DesktopPath\fixed_obs\win-capture.dll" -Destination "C:\Program Files\obs-studio\obs-plugins\64bit\" -Force
-Move-Item -Path "$DesktopPath\fixed_obs\graphics-hook32.dll" -Destination "C:\Program Files\obs-studio\data\obs-plugins\win-capture\graphics-hook32.dll" -Force
+if ([System.IO.File]::Exists($destDLLgh)) {Remove-Item -Path $destDLLgh -Force}
+if ([System.IO.File]::Exists($destDLLwc)) {Remove-Item -Path $destDLLwc -Force}
+
+Expand-Archive -Path "$DesktopPath\fixed_obs.zip" -DestinationPath $DesktopPath -Force
+Move-Item -Path "$DesktopPath\fixed_obs\win-capture.dll" -Destination $destDLLwc -Force
+Move-Item -Path "$DesktopPath\fixed_obs\graphics-hook32.dll" -Destination $destDLLgh -Force
 Write-Progress "DLLs moved, patch applied, finishing up"
 
 Remove-Item -Path "$DesktopPath\obs-studio-hook.zip" -Force
